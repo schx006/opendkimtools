5f42343fe635470b3d59b3fa752b2355edaa3123
schx006 (S.X.) <schx006@xs-net.com>
2024-02-22 16:44:57 +0100

    Update newselectordictionary.py

--- newselectordictionary.py
+++ newselectordictionary.py
@@ -9,38 +9,43 @@ import re, random
 
 # Read a list of words in a file.
 def readList(myNameList):
-    with open(myNameList, encoding="utf-8") as f:
-        myList = f.readlines()
-    for i in range(len(myList)):
-        myList[i] = re.sub(r'\n', '', myList[i])
-    return(myList)
+	with open(myNameList, encoding="utf-8") as f:
+	myList = f.readlines()
+	for i in range(len(myList)):
+		myList[i] = re.sub(r'\n', '', myList[i])
+		return(myList)
 
 
 # Randomly return 3 items from a list.
 def newSelectors(myList):
-    return(random.sample(myList, 3))
+	return(random.sample(myList, 3))
 
 
-
+# Create a dictionary structure for domain names and
+# a list of their selectors.
 def newSelectorDictionnary():
-    domain = readList('/root/domainList.txt')
-    for dn in domain:
-        print(dn)           # INTRUMENTATION
-        match dn:
-            case 'decadent.art':
-                nameList = '/root/us-state-capitals.txt'
-            case 'schoepfer.nom.fr':
-                nameList = '/root/fr-regional-capitals.txt'
-            case _:
-                nameList = '/root/eu-capitals.txt'
-        print(nameList)     # INTRUMENTATION
-        #print(getPublicKey())
-        selector = newSelectors(readList(nameList))
-        print(newSelector)  # INSTRUMENTATION
+	# Read the list of domain names.
+	domain = readList('assets/domainList.txt')
+	# For each domain, choose a list of words from which
+	# the names of the selectors are randomly drawn.
+	for dn in domain:
+		print(dn)	# INTRUMENTATION
+		match dn:
+			case 'decadent.art':
+				nameList = 'assets/us-state-capitals.txt'
+			case 'schoepfer.nom.fr':
+				nameList = 'assets/fr-regional-capitals.txt'
+			case _:
+				nameList = 'assets/eu-capitals.txt'
+	print(nameList)		# INTRUMENTATION
+	# Create a list of selector names
+	selector = newSelectors(readList(nameList))
+	print(newSelector)	# INSTRUMENTATION
+
 
 
 def main():
-    newSelectorDictionnary()
+	newSelectorDictionnary()
 
 
 
